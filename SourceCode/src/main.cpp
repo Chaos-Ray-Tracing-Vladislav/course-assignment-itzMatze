570d924268644207379bd1220e2fa16ae097f25e
Matthias Bruns <matthiasbruns35@gmail.com>
2024-05-30 18:06:02 +0200

    enable saving a vector of hex pixel values to a file

---
+++ SourceCode/src/main.cpp
@@ -0,0 +1,67 @@
+#include <cstdint>
+#include <string>
+#include <vector>
+#include <ctime>
+#include <fstream>
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "stb_image_write.h"
+
+constexpr uint32_t image_width = 1920;
+constexpr uint32_t image_height = 1080;
+
+enum class FileType
+{
+  ppm,
+  png
+};
+
+// pixel hex values are assumed to be in abgr format
+void save_image(std::vector<uint32_t> pixels, const std::string& name, int width, int height, FileType type = FileType::png)
+{
+  std::string image_path("../../Images/" + name);
+  if (!name.empty())
+  {
+    image_path.append("_");
+  }
+  // add current time to filename
+  std::time_t time = std::time(nullptr);
+  std::tm local_time = *std::localtime(&time);
+  char time_chars[20];
+  std::strftime(time_chars, sizeof(time_chars), "%d-%m-%Y_%H-%M-%S", &local_time);
+  std::string time_string(time_chars);
+  image_path.append(time_string);
+  if (type == FileType::png)
+  {
+    image_path.append(".png");
+    stbi_write_png(image_path.c_str(), width, height, 4, pixels.data(), width * 4);
+  }
+  else if (type == FileType::ppm)
+  {
+    image_path.append(".ppm");
+    std::ofstream file(image_path, std::ios::out | std::ios::binary);
+    file << "P3\n" << width << " " << height << "\n" << 255 << "\n";
+    for (uint32_t y = 0; y < height; y++)
+    {
+      for (uint32_t x = 0; x < width; x++)
+      {
+        // extract rgb values from hex representation and print to the file
+        uint32_t pixel = pixels[y * width + x];
+        file << (pixel & 0x000000ff) << " ";
+        pixel >>= 8;
+        file << (pixel & 0x000000ff) << " ";
+        pixel >>= 8;
+        file << (pixel & 0x000000ff) << "\t";
+      }
+    file << "\n";
+    }
+  file.close();
+  }
+}
+
+int main(int argc, char** argv)
+{
+  std::vector<uint32_t> pixels(image_width * image_height, 0xffff00ff);
+  save_image(pixels, "", image_width, image_height, FileType::png);
+  return 0;
+}
+
