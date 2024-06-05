d9e3673dbb0ee3142550a62ce3fd67106753c878
Matthias Bruns <matthiasbruns35@gmail.com>
2024-06-05 22:54:35 +0200

    add triangle class

---
+++ SourceCode/include/triangle.hpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "vec.hpp"
+
+class Triangle
+{
+  Triangle() = default;
+  Triangle(const cm::Vec3& v0, const cm::Vec3& v1, const cm::Vec3& v2);
+
+  cm::Vec3 v0;
+  cm::Vec3 v1;
+  cm::Vec3 v2;
+};
+
---
+++ SourceCode/src/triangle.cpp
@@ -0,0 +1,4 @@
+#include "triangle.hpp"
+
+Triangle::Triangle(const cm::Vec3& v0, const cm::Vec3& v1, const cm::Vec3& v2) : v0(v0), v1(v1), v2(v2) {}
+
