b8cf771b4414f6e1869aaf7be04cf4406614b907
Matthias Bruns <matthiasbruns35@gmail.com>
2024-06-10 17:27:22 +0200

    improve readability of camera class

--- SourceCode/include/camera.hpp
+++ SourceCode/include/camera.hpp
@@ -2,11 +2,22 @@
 #include "vec.hpp"
 #include "ray.hpp"
 
+/*
+ * ray tracing camera that can generate rays for each pixel
+ * resolution of the image that the rays are generated for
+ * orientation of the camera consisting of origin, view direction, and up vector
+ * camera parameters that define the pinhole camera
+*/
 class Camera
 {
 public:
   Camera() = default;
-  Camera(const cm::Vec2u& resolution, const cm::Vec3& origin = cm::Vec3(0.0, 0.0, 0.0), const cm::Vec3& view_dir = cm::Vec3(0.0, 0.0, -1.0), const cm::Vec3& up = cm::Vec3(0.0, 1.0, 0.0), float sensor_width = 0.036 /* standard full frame sensor width 36mm */, float focal_length = 0.03 /* 30mm camera lens */);
+  Camera(const cm::Vec2u& resolution,
+         const cm::Vec3& origin = cm::Vec3(0.0, 0.0, 0.0),
+         const cm::Vec3& view_dir = cm::Vec3(0.0, 0.0, -1.0),
+         const cm::Vec3& up = cm::Vec3(0.0, 1.0, 0.0),
+         float sensor_width = 0.036 /* standard full frame sensor width 36mm */,
+         float focal_length = 0.03 /* 30mm camera lens */);
 
   Ray get_ray(const cm::Vec2u& pixel, bool use_jittering = false);
 
@@ -15,7 +26,7 @@ private:
   // position of the camera sensor
   cm::Vec3 upper_left_corner;
   float pixel_size;
-  // camera coordinate system: right, up, backwards in camera perspective
-  cm::Vec3 u, v, w;
+  // camera coordinate system
+  cm::Vec3 right, up, backward;
 };
 
