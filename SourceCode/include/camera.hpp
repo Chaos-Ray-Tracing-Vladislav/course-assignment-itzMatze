010ea1cfc57414f120de21f26aea55a947b88524
Matthias Bruns <matthiasbruns35@gmail.com>
2024-06-11 19:20:30 +0200

    move camera parameters into struct

--- SourceCode/include/camera.hpp
+++ SourceCode/include/camera.hpp
@@ -2,24 +2,27 @@
 #include "vec.hpp"
 #include "ray.hpp"
 
+struct CameraConfig
+{
+  const cm::Vec3 origin = cm::Vec3(0.0, 0.0, 0.0);
+  const cm::Vec3 view_dir = cm::Vec3(0.0, 0.0, -1.0);
+  const cm::Vec3 up = cm::Vec3(0.0, 1.0, 0.0);
+  float focal_length = 0.03; // 30mm camera lens
+  float sensor_width = 0.036; // standard full frame sensor width 36mm
+};
+
 /*
  * ray tracing camera that can generate rays for each pixel
  * resolution of the image that the rays are generated for
- * orientation of the camera consisting of origin, view direction, and up vector
- * camera parameters that define the pinhole camera
+ * orientation and parameters of the camera
 */
 class Camera
 {
 public:
   Camera() = default;
-  Camera(const cm::Vec2u& resolution,
-         const cm::Vec3& origin = cm::Vec3(0.0, 0.0, 0.0),
-         const cm::Vec3& view_dir = cm::Vec3(0.0, 0.0, -1.0),
-         const cm::Vec3& up = cm::Vec3(0.0, 1.0, 0.0),
-         float sensor_width = 0.036 /* standard full frame sensor width 36mm */,
-         float focal_length = 0.03 /* 30mm camera lens */);
+  Camera(const cm::Vec2u& resolution, const CameraConfig& config = CameraConfig());
 
-  Ray get_ray(const cm::Vec2u& pixel, bool use_jittering = false);
+  Ray get_ray(const cm::Vec2u pixel, bool use_jittering = false);
 
 private:
   cm::Vec3 origin;
--- SourceCode/src/camera.cpp
+++ SourceCode/src/camera.cpp
@@ -3,20 +3,20 @@
 #include "vec.hpp"
 #include "random_generator.hpp"
 
-Camera::Camera(const cm::Vec2u& resolution, const cm::Vec3& origin, const cm::Vec3& view_dir, const cm::Vec3& up, float sensor_width, float focal_length)
-  : origin(origin), backward(cm::normalize(-view_dir))
+Camera::Camera(const cm::Vec2u& resolution, const CameraConfig& config)
+  : origin(config.origin), backward(cm::normalize(-config.view_dir))
 {
   // camera coordinate system
-  right = cm::normalize(cm::cross(up, backward));
-  this->up = cm::cross(backward, right);
+  right = cm::normalize(cm::cross(config.up, backward));
+  up = cm::cross(backward, right);
   // match sensor aspect ratio to resolution aspect ratio
-  float sensor_height = (float(resolution.y) / float(resolution.x)) * sensor_width;
+  float sensor_height = (float(resolution.y) / float(resolution.x)) * config.sensor_width;
   // pixel size is always quadratic: (sensor_height / float(resolution.y) = sensor_width / float(resolution.x))
-  pixel_size = sensor_width * (1.0 / float(resolution.x));
-  upper_left_corner = origin + (sensor_height / 2.0) * up - (sensor_width / 2.0) * right - focal_length * backward;
+  pixel_size = config.sensor_width * (1.0 / float(resolution.x));
+  upper_left_corner = origin + (sensor_height / 2.0) * up - (config.sensor_width / 2.0) * right - config.focal_length * backward;
 }
 
-Ray Camera::get_ray(const cm::Vec2u& pixel, bool use_jittering)
+Ray Camera::get_ray(const cm::Vec2u pixel, bool use_jittering)
 {
   // offset to either get a random position inside of the pixel square or the center of the pixel
   cm::Vec2 offset = use_jittering ? cm::Vec2(rng::random_float(), rng::random_float()) : cm::Vec2(0.5);
