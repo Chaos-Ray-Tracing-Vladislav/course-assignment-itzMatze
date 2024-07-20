5e6a05d79ed8ae244bcbe4718cdeb3b736b7c8cb
Matthias Bruns <matthiasbruns35@gmail.com>
2024-07-20 22:40:20 +0200

    avoid unnecessary matrix construction in camera config constructor

--- SourceCode/src/renderer/camera.cpp
+++ SourceCode/src/renderer/camera.cpp
@@ -4,7 +4,7 @@
 #include "util/vec.hpp"
 
 CameraConfig::CameraConfig() :
-  spatial_conf(cm::rotate(cm::Vec3(0.0, 0.0, 0.0)), cm::Vec3(0.0, 0.0, 0.0)),
+  spatial_conf(cm::Vec3(0.0, 0.0, 0.0)),
   focal_length(0.03) /* 30mm camera lens */,
   sensor_size(0.036) /* standard full frame sensor width 36mm */
 {}
